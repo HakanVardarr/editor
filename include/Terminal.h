c3376a37fbc0521c934ccc07a72b019f174c9742
Hakan Vardar <hakovardo123@gmail.com>
2024-02-25 21:41:21 +0300

    Restart project

--- include/Terminal.h
+++ include/Terminal.h
@@ -6,15 +6,6 @@
 #include "Windows.h"
 #include <string>
 
-#define CLEANUP_AND_RETURN(message) \
-    Cleanup(message);               \
-    bRun = false;                   \
-    return;
-
-#define CLEANUP_AND_RETURN_FALSE(message) \
-    Cleanup(message);                     \
-    return false;
-
 class Terminal
 {
 public:
@@ -25,27 +16,7 @@ public:
     Terminal &operator=(Terminal &&) = delete;
     ~Terminal();
 
-    VOID Exit();
-    VOID UpdateCursor(COORD pos);
-
-    BOOL Clear();
-    BOOL Run();
-    BOOL MoveCursor(COORD pos);
-    BOOL Write(std::string content, DWORD *written);
-
-    COORD CursorPosition();
-    COORD TerminalSize();
-
-    INPUT_RECORD ReadInput();
-
 private:
-    HANDLE hStdin, hStdout;
-    DWORD dwOldMode, dwMode;
-    BOOL bRun = true;
-    COORD cPos = {0, 0}, cSize;
-
-    VOID Cleanup(std::string sMessage);
-    HANDLE CreateConsoleBuffer();
 };
 
 #else
